1cb4e001a5d2b78c4c3192db1594996de8dd451b
terry <terry3t@gmail.com>
2025-12-24 14:08:49 +0800

    update config

--- worker.ts
+++ worker.ts
@@ -1,20 +1,18 @@
 export default {
-    async fetch(request: Request, env: any) {
-      const url = new URL(request.url);
-  
-      // 先尝试返回真实静态资源
-      let res = await env.ASSETS.fetch(request);
-  
-      // 如果没找到，则返回 SPA 入口 index.html
-      if (res.status === 404) {
-        const indexReq = new Request(
-          new URL('/index.html', url).toString(),
-          request
-        );
-        res = await env.ASSETS.fetch(indexReq);
-      }
-  
-      return res;
-    },
-  };
-  
+  async fetch(request: Request, env: any) {
+    const url = new URL(request.url);
+
+    let res = await env.ASSETS.fetch(request);
+
+    if (
+      res.status === 404 &&
+      !url.pathname.startsWith('/assets/')
+    ) {
+      const indexUrl = new URL('/index.html', url);
+      const indexReq = new Request(indexUrl.toString(), request);
+      res = await env.ASSETS.fetch(indexReq);
+    }
+
+    return res;
+  },
+};
